a1f1280f81f108e251bcf144884a7b1d66e82263
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-06-11 00:25:45 -0700

    Add the maximum rectangle area

--- src/main3.cpp
+++ src/main3.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 int main()
 { 
-    Rectangle rect;
+    Rectangle rect, rect1;
+
     rect.set_width(15);
+    rect1.set_width(50);
+
     rect.set_height(30); 
+    rect1.set_height(30);
+
     cout << "Standard rectangle area: " << rect.area() << endl;
+    cout << "Maximum rectangle area: " << rect1.area() << endl;
     return 0;
 }
 
